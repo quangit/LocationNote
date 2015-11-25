ce03e766f05f60cc627f5aff619c71c2af921a39
quangit <quang11t1@outlook.com>
2015-11-25 11:01:36 +0700

    update actionbar,Login

--- app/src/main/java/com/example/quang11t1/locationnote/activity/Login.java
+++ app/src/main/java/com/example/quang11t1/locationnote/activity/Login.java
@@ -10,6 +10,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -33,6 +35,8 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
        // getActionBar().setDisplayHomeAsUpEnabled(true);
         Button button = (Button) findViewById(R.id.button_login);
         final EditText editText_user =(EditText) findViewById(R.id.editText_user);
@@ -70,6 +74,32 @@ public class Login extends AppCompatActivity {
         };
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.secound_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            Toast.makeText(this,"back",Toast.LENGTH_SHORT).show();
+            sendToMain(1,null);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
     public  void saveInforLogin(Account account)
     {
         try{
