976108c8a3ce8db74c2d4e4800e352efe75b69e2
langtubs298 <luongvien.binhson@gmail.com>
2015-11-25 21:54:19 +0700

    hihie

--- app/src/main/java/com/example/quang11t1/locationnote/activity/LocationNoteList.java
+++ app/src/main/java/com/example/quang11t1/locationnote/activity/LocationNoteList.java
@@ -1,5 +1,6 @@
 package com.example.quang11t1.locationnote.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.example.quang11t1.locationnote.R;
